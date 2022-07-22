9603517c2305d522aa33c48bc8c21e86fb736be8
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-07-22 22:09:21 +0700

    Adding CDN Bootstrap and Setup Redux

---
+++ src/Dice-Game/GameInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GameInfo extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default GameInfo;
